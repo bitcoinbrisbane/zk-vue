b53275ca004f777da22d371804a5fbf02534949b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-11-18 12:20:57 +1000

    refactor: remove AboutView, add FirstView and update HomeView layout

---
+++ src/views/First.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+<!-- <script>
+// export default {
+//   name: "LatheBufferGeometry",
+//   mounted() {
+//     if (
+//       navigator.userAgent.match(
+//         /(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i
+//       )
+//     ) {
+//       this.$router.push({ path: "/mobile" });
+//     } else {
+//       this.$router.push({ path: "/page" });
+//     }
+//   },
+// };
+<script> -->
+<script>
+export default {
+    name: 'First',
+
+}
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="home">
+        
+            <!-- <top-bar v-if="permission.length===0" /> -->
+            <router-view class="home-body" />
+   
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'Home',
+
+}
+</script>
+<style lang="scss">
+    .home {
+        width: 100%;
+         //overflow-x: hidden;
+        .home-content {
+            width: calc(100% - 140px);
+            position: relative;
+            flex-grow: 1;
+            .home-body{
+                background: #F8F9FC;
+                width: 100%;
+                overflow-x: hidden;
+            }
+        }
+    }
+</style>
